4ee39ac294d735847780404f4c874e71a80a6e44
Mingpeng Chen <mchen88@ucsc.edu>
2022-04-11 23:39:23 -0700

    addition

---
+++ .ipynb_checkpoints/calculation-checkpoint.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def addition(x, y):
+    pass
+
+
+# In[2]:
+
+
+def substraction(x, y):
+    pass
+
+
+# In[3]:
+
+
+def multiplication(x, y):
+    pass
+
+
+# In[4]:
+
+
+def division(x, y):
+    pass
+
+
+# In[ ]:
+
+
+
+
---
+++ calculation.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+def addition(x, y):
+    return x + y
+
+
+# In[2]:
+
+
+def substraction(x, y):
+    pass
+
+
+# In[3]:
+
+
+def multiplication(x, y):
+    pass
+
+
+# In[4]:
+
+
+def division(x, y):
+    pass
+
+
+# In[ ]:
+
+
+
+
